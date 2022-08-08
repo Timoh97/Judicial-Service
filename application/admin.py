5381dba3460b0cf591577fc30aaa7ceb5c97aee7
Timoh97 <timohmugendi@gmail.com>
2022-08-08 16:44:40 +0300

    register models in admin.py

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
+from django.utils.translation import ngettext
+
+# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Contact)
+admin.site.register(Post)
+admin.site.register(Court)
+admin.site.register(User)
 
 # Register your models here.
